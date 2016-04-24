4977537ccefaa4ff9ee19f454f51be876578c016
PhucHuynhTan <phuchtgc60244@fpt.edu.vn>
2016-04-24 13:20:09 +0700

    update source code

--- PersonalTravelApplication/app/src/main/java/htp/personaltravelapplication/AddActivity.java
+++ PersonalTravelApplication/app/src/main/java/htp/personaltravelapplication/AddActivity.java
@@ -1,5 +1,6 @@
 package htp.personaltravelapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
@@ -8,42 +9,45 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import htp.personaltravelapplication.bean.MapObject;
+import htp.personaltravelapplication.bean.ObjectList;
 
 public class AddActivity extends AppCompatActivity {
     EditText eTitle, eContent;
     TextView tLng,tLat, tAddress;
-    Button btn;
+    MenuItem btn;
     LocationManager lManager;
     String provide;
     Location location;
     TableController tableController;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        context = AddActivity.this.getBaseContext();
         eTitle=(EditText)findViewById(R.id.txt_Title);
         eContent=(EditText) findViewById(R.id.txt_Content);
         tAddress =(TextView)findViewById(R.id.lblAddress);
         tLng=(TextView)findViewById(R.id.lblLng);
         tLat=(TextView)findViewById(R.id.lbl_Lat);
-        btn=(Button)findViewById(R.id.action_add);
+        btn=(MenuItem)findViewById(R.id.action_add);
+
         Intent i = getIntent();
         MapObject mapObject = (MapObject)i.getSerializableExtra("mapObject");
         tLat.setText(String.valueOf(""+mapObject.getLat()));
         tLng.setText(String.valueOf("" + mapObject.getLng()));
         tAddress.setText(mapObject.getAddress());
         tableController = new TableController(AddActivity.this.getBaseContext());
-
-
-
-
     }
 
     @Override
@@ -53,19 +57,29 @@ public class AddActivity extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
-
     /* On selecting action bar icons
     * */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Take appropriate action for each action item click
-
         switch (item.getItemId()) {
-            case R.id.action_add:
-                // add action
-                btn=(Button)findViewById(R.id.action_add);
-                btn.setOnClickListener(new OnClickListenerAddContent());
-                return true;
+            case  R.id.action_add:
+                String title = AddActivity.this.eTitle.getText().toString();
+                String content = AddActivity.this.eContent.getText().toString();
+                String lng = AddActivity.this.tLng.getText().toString();
+                String lat = AddActivity.this.tLat.getText().toString();
+                ObjectList objList = new ObjectList();
+                objList.title = title;
+                objList.content = content;
+                objList.lng = lng;
+                objList.lat = lat;
+                boolean result = AddActivity.this.tableController.createListItem(objList);
+                if (result) {
+                    Toast.makeText(context, "Create List successfull", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Failed to create note", Toast.LENGTH_SHORT).show();
+                }
+                return false;
             default:
                 return super.onOptionsItemSelected(item);
         }
--- PersonalTravelApplication/app/src/main/java/htp/personaltravelapplication/OnClickListenerAddContent.java
+++ PersonalTravelApplication/app/src/main/java/htp/personaltravelapplication/OnClickListenerAddContent.java
@@ -16,15 +16,15 @@ import htp.personaltravelapplication.bean.ObjectList;
  * Created by phuchtgc60244 on 4/12/2016.
  */
 public class OnClickListenerAddContent implements View.OnClickListener {
-
+    Button btnAdd;
     @Override
     public void onClick(View v) {
         final Context context = v.getContext();
         // Toast.makeText(context, "View add", Toast.LENGTH_SHORT).show();
-        String title = ((AddListActivity) context).eTitle.getText().toString();
-        String content = ((AddListActivity) context).eContent.getText().toString();
-        String lng = ((AddListActivity) context).tLng.getText().toString();
-        String lat = ((AddListActivity) context).tLat.getText().toString();
+        String title = ((AddActivity) context).eTitle.getText().toString();
+        String content = ((AddActivity) context).eContent.getText().toString();
+        String lng = ((AddActivity) context).tLng.getText().toString();
+        String lat = ((AddActivity) context).tLat.getText().toString();
         ObjectList objList = new ObjectList();
         objList.title = title;
         objList.content = content;
@@ -32,7 +32,7 @@ public class OnClickListenerAddContent implements View.OnClickListener {
         objList.lat = lat;
 
 
-        boolean result = ((AddListActivity) context).tableController.createListItem(objList);
+        boolean result = ((AddActivity) context).tableController.createListItem(objList);
         if (result) {
 
             Toast.makeText(context, "Create List successfull", Toast.LENGTH_SHORT).show();
